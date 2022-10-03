557938ff2b3840d06f5c21cc8c5fbea808e331b8
Nabster2010 <58792560+Nabster2010@users.noreply.github.com>
2022-10-03 04:46:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import i18n from "./i18n";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+
+  i18n.on("languageChanged", (locale) => {
+    const direction = i18n.dir(locale);
+    console.log(direction);
+    document.dir = direction;
+  });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
